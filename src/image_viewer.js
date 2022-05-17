f1d821a4fafd45d5d2901a5f8ead2e958a004538
ediloaz <ediloaz@gmail.com>
2022-05-17 11:46:38 -0600

    add code splitting and on demand code loading

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -4,13 +4,15 @@ import big from '../assets/big.jpg';
 import '../styles/image_viewer_3.css'
 import '../styles/image_viewer_2.css'
 
-const image = document.createElement('img');
-image.src = small;
-// image.src = "./../assets/image-400x400.jpg";
-document.body.appendChild(image);
 
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
+    // image.src = "./../assets/image-400x400.jpg";
+    document.body.appendChild(image);
+    
+    const span = document.createElement('span');
+    span.innerHTML = "I'm a span 😀🤣";
+    document.body.appendChild(span);
+}
 
-
-const span = document.createElement('span');
-span.innerHTML = "I'm a span 😀🤣";
-document.body.appendChild(span);
